ac5f4d888aa8b69678784bedb394b3a4b60e8522
Diego Gettatelli <d.gettatelli@solvingteam.it>
2022-09-29 15:57:00 +0200

    struttura principale, inizio implementazione header e stili

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './common/header/header.component';
+import { MenuComponent } from './common/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
